056977f7082a8ce59d04649b8c00604e34be5a5f
umachowska <38352697+umachowska@users.noreply.github.com>
2018-04-17 10:48:58 +0200

    Add files via upload

---
+++ Answer Destination Mars/mission 7/7_15/7_15.ino
@@ -0,0 +1,39 @@
+#include <Skribot.h>
+ 
+  Skribot robot;                                  
+  int LeftSensor,RightSensor; 
+                      
+  void setup() {
+
+  robot.AddDCRotor("LEFT");          
+  robot.AddDCRotor("RIGHT");
+
+  robot.AddLED("LED1");
+  robot.AddLED("LED2");
+
+  robot.SetSpeed(250);   
+  
+  robot.AddDistSensor("D1");        
+  robot.AddDistSensor("D2");
+
+}
+
+void loop() {
+   LeftSensor = robot.ReadDistSensor("D1");      
+   RightSensor = robot.ReadDistSensor("D2");  
+
+   
+   robot.MoveForward(200);
+   
+   while(LeftSensor < 20 || RightSensor < 20){
+      robot.TurnLEDOn(255, 0, 0);
+      LeftSensor = robot.ReadDistSensor("D1");      
+      RightSensor = robot.ReadDistSensor("D2");    
+   }
+   
+   robot.TurnLEDOff();
+
+    
+    
+   
+}
