b74fdc40c41fe7f2c9f16b77d2b49d1ac15dd60c
umachowska <38352697+umachowska@users.noreply.github.com>
2018-04-17 11:10:13 +0200

    Add files via upload

---
+++ Answer Destination Mars/mission 8/8_4/8_4.ino
@@ -0,0 +1,24 @@
+#include <Skribot.h>
+ 
+  Skribot robot;                      
+       
+void setup() {
+  robot.AddDCRotor("LEFT");
+  robot.AddDCRotor("RIGHT");
+  
+  robot.SetSpeed(250);             
+  robot.AddClaw();
+
+  robot.MoveForward(2000);
+  robot.CloseClaw();
+  robot.Pick_Up(); 
+
+  robot.MoveBack(2000);
+  robot.Put_Down();                                     
+  robot.OpenClaw();
+  
+}
+
+void loop() {
+ 
+}
